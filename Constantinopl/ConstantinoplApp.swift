43222b42568ce264fc77b4d8c18630b77aefe1ef
synocode <synoplex33@gmail.com>
2023-07-17 08:48:41 -0700

    Initial Commit

---
+++ Constantinopl/ConstantinoplApp.swift
@@ -0,0 +1,17 @@
+//
+//  ConstantinoplApp.swift
+//  Constantinopl
+//
+//  Created by Forest Hughes on 7/17/23.
+//
+
+import SwiftUI
+
+@main
+struct ConstantinoplApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
